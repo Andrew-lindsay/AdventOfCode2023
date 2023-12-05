5dbf8c7e138d3183706d74162110d1a2d8ab80a4
Andrew Lindsay <andrewlindsay97@gmail.com>
2023-12-05 00:37:11 +0000

    - Adding logic for reading the file and finding the the first and last digit in each line and summing up the result

--- DayOne/Program.cs
+++ DayOne/Program.cs
@@ -1,7 +1,64 @@
-﻿namespace AdventOfCode2023.DayOne {
+﻿using System.Reflection;
+using System.Reflection.PortableExecutable;
+
+namespace AdventOfCode2023.DayOne {
     public class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            string filePath = @"Inputs\input";
+
+            if (!File.Exists(filePath)) {
+                Console.WriteLine("File Not Found");
+                Environment.Exit(-1);
+            }
+
+            using StreamReader file = File.OpenText(filePath);
+
+            string? line = null;
+            int count = 0;
+            List<int> results = new List<int>();
+            while ((line = file.ReadLine()) != null) {
+                count++;
+                char? firstDigit = null;
+                char? secondDigit = null;
+
+                int leftIndex = 0;
+                for (leftIndex = 0; leftIndex < line.Length; leftIndex++) {
+                    char c = line[leftIndex];
+
+                    if (!Char.IsDigit(c)) {
+                        continue;
+                    }
+
+                    firstDigit = c;
+                    break;
+                }
+
+                for (int rightIndex = line.Length - 1; rightIndex >= leftIndex; rightIndex--) {
+                    char c = line[rightIndex];
+
+                    if (!Char.IsDigit(c)) {
+                        continue;
+                    }
+
+                    secondDigit = c;
+                    break;
+                }
+
+                if (firstDigit == null && secondDigit == null) {
+                    Console.WriteLine($"No digits found in line {count}");
+                    continue;
+                }
+
+                if (firstDigit != null && secondDigit == null) {
+                    secondDigit = firstDigit;
+                }
+
+                int lineResult = int.Parse($"{firstDigit}{secondDigit}");
+                results.Add(lineResult);
+            }
+
+            Console.WriteLine($"Total: {results.Sum()}");
         }
     }
 }
